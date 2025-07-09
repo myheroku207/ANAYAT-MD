38e8d351391eb2d58c39f28080d9480f06134e00
ANAYAT-KHAN-MD <anayatullahmdbot@gmail.com>
2025-07-09 20:19:28 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -15,13 +15,13 @@ let dec = `*👋 Hello ${pushname}*
 
 > *MY OWNER INFO 👨‍💻* 
 
-*🔥 ᴏᴡɴᴇʀ ɴᴀᴍᴇ -: Arslan-Ultra-MD*
-🔥 *ɴᴜᴍʙᴇʀ* -: 923237045919
-🔥 *ᴡʜᴀᴛꜱᴀᴘᴘ ᴄʜᴀɴɴᴇʟ-:* https://whatsapp.com/channel/0029VarfjW04tRrmwfb8x306
+*🔥 ᴏᴡɴᴇʀ ɴᴀᴍᴇ -: ANAYAT-MD*
+🔥 *ɴᴜᴍʙᴇʀ* -: 923452401207
+🔥 *ᴡʜᴀᴛꜱᴀᴘᴘ ᴄʜᴀɴɴᴇʟ-:* https://whatsapp.com/channel/0029VbAm8LqL2ATpxklIct2g
 
 *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ  ArslanMD Official*
 `
-await conn.sendMessage(from,{image:{url: `https://files.catbox.moe/atby2t.png`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://files.catbox.moe/whkzda.jpg`},caption:dec},{quoted:mek});
 
 }catch(e){
 console.log(e)
