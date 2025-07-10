380853f2671ec31b12199e06e0bfd535bba09ca5
ANAYAT-KHAN-MD <anayatullahmdbot@gmail.com>
2025-07-10 16:36:52 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ARSL~8v9SSQJR#tsYggH0uu780lz81gh9NmTjE2_vz0CtOmV7YuCofD5I",  // ADD YOUR SESSION ID 
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/atby2t.png",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/whkzda.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that ANAYAT-MD is ALIVE now—ready to dive into new adventures with you!",
 SUDO_NB: process.env.SUDO_NB || "923452401207",
 AUTO_REACT: process.env.AUTO_REACT || "true", // Enable/disable auto reaction (true/false)
